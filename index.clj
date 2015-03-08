fc45bc5a673d0f4516cc9d552e54485de301e2f7
snakevash <164011252@qq.com>
2015-03-08 09:32:32 +0800

    到let部分

--- index.clj
+++ index.clj
@@ -1,19 +1,72 @@
-(prn '测试一下 snake lee')
-(prn 'snake')
-(prn 'czq')
-(prn '世界 lee!')
+;; 赋值语句
+;; (= var value [... var value])
+;; (= var)
 
+(= newvar "赋值一个字符窜")
+(= pi (if (is 2 3)
+        NaN
+        Math.PI))
 
-; (def <name> [<... params>] [<body>])
-; 标准函数定义
+;; 赋值可以多条件
+(= lastest (= first "coffee"
+              second "Beer"
+              third "Milk"))
+
+;; 赋值语句可以正确处理代码块
+(= x (= shifty null
+        picky (if false "nose up" "smile")
+        dodgy (try (+ something) "unsuccessful")))
+
+;; 单个=就是定义
+(= emptyVar)
+
+;; 引用
+`(1 2 3)
+(quote (1 2 3))
+`myvar
+`'mystring'
+
+;; 没有语法quote
+(+ (^ 2 1) (^ 3 2))
+`(+ (^ 2 1) (^ 3 2))
+
+;; 解引用
+`((* 2 3) (+ 4 5))
+`(,(* 2 3) ,(+ 4 5))
+
+;; 命名函数
+;; (def <name> [<... params>] [<body>])
 (def fact x
-	(if (is x 0) 1
-		(* x (fact (- x 1)))))
-(prn (fact 5))
+  (if (is x 0)
+    1
+    (* x (fact (- x 1)))))
 
-; 匿名函数定义
-; (fn [<... params>] [<body>])
-(prn `测试匿名函数： ((fn first second (+ first second)) 1 2))
+;; 匿名函数
+;; (fn [<... params>] [<body>])
+(fn first second
+  (+ first second))
 
-(= test (require './src/snake'))
-(console.log (test.add 1 2))
+;; 调用和返回
+(= ringMyBell (fn bell
+                (prn bell)))
+(ringMyBell "\x07")
+((fn x (+ "hello " x)) "World!")
+
+;; 函数返回值
+(def numerify x
+  (if (isNaN (Number x))
+    (do (prn "not a number:" x)
+      NaN)
+    (Number x)))
+
+;; #(NYI) 调用arguments
+((fn (* #0 #2)) 3 100 4 2)
+
+;; {<body>}
+{alert "It's a dragon!"}
+{}
+
+;; let 局部代码块 用函数来实现的
+;; (let [var value [var value ...]] [<body>])
+
+
--- index.js
+++ index.js
@@ -1,18 +1,64 @@
 (function() {
-  var test;
-  console.log('测试一下 snake lee');
-  console.log('snake');
-  console.log('czq');
-  console.log('世界 lee!');
+  var newvar, pi, first, second, third, lastest, shifty, picky, dodgy, x, emptyVar, ringMyBell, _ref, _err;
+  newvar = "赋值一个字符窜";
+  pi = ((2 === 3) ? NaN : Math.PI);
+  first = "coffee";
+  second = "Beer";
+  lastest = (third = "Milk");
+  shifty = null;
+  picky = (false ? "nose up" : "smile");
+  try {
+    _ref = something;
+  } catch (_err) {
+    _ref = "unsuccessful";
+  }
+  x = (dodgy = _ref);
+  emptyVar;
+  [1, 2, 3];
+  [1, 2, 3];
+  "myvar";
+  "'mystring'";
+  (2 ^ 1) + (3 ^ 2);
+  [+, [ ^ , 2, 1],
+    [ ^ , 3, 2]
+  ];
+  [
+    [ * , 2, 3],
+    [+, 4, 5]
+  ];
+  [2 * 3, 4 + 5];
 
   function fact(x) {
     return ((x === 0) ? 1 : (x * fact(x - 1)));
   }
   fact;
-  console.log(fact(5));
-  console.log("测试匿名函数：", (function(first, second) {
+  (function(first, second) {
     return (first + second);
-  })(1, 2));
-  test = require('./src/snake');
-  return console.log(test.add(1, 2));
+  });
+  ringMyBell = (function(bell) {
+    return console.log(bell);
+  });
+  ringMyBell("\x07");
+  (function(x) {
+    return ("hello " + x);
+  })("World!");
+
+  function numerify(x) {
+    var _ref0;
+    if (isNaN(Number(x))) {
+      console.log("not a number:", x);
+      _ref0 = NaN;
+    } else {
+      _ref0 = Number(x);
+    }
+    return _ref0;
+  }
+  numerify;
+  (function() {
+    return (arguments[0] * arguments[2]);
+  })(3, 100, 4, 2);
+  (function() {
+    return alert("It's a dragon!");
+  });
+  return (function() {});
 })['call'](this);
