76c8611d7a2c85c1debc6385e4c0cd10ad8b23ca
snakevash <164011252@qq.com>
2015-03-23 14:09:35 +0800

    引用

---
+++ src/quoting.js
@@ -0,0 +1,16 @@
+(function() {
+  1(2, 3);
+  [1, 2, 3];
+  [1, 2, 3];
+  "myvar";
+  "'mystring'";
+  (2 ^ 1) + ("a" === "b");
+  [+, [ ^ , 2, 1],
+    [is, "a", "b"]
+  ];
+  [
+    [ * , 2, 3],
+    [is, true, false]
+  ];
+  return [2 * 3, true === false];
+})['call'](this);
